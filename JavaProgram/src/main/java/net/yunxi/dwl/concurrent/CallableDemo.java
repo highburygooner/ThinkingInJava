416ee2e9721d664636964d0c64eab4fa597c8da9
highburygooner <234272598@qq.com>
2016-07-20 10:24:34 +0800

    addnew

---
+++ JavaProgram/src/main/java/net/yunxi/dwl/concurrent/CallableDemo.java
@@ -0,0 +1,55 @@
+package net.yunxi.dwl.concurrent;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class TaskWithResult implements Callable{
+
+	private int id;
+	
+	public TaskWithResult(int id) {
+		super();
+		this.id = id;
+	}
+
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return "Result of Task "+id;
+	}
+	
+	
+}
+
+public class CallableDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExecutorService exex=Executors.newCachedThreadPool();
+		List<Future<String>> results=new ArrayList<Future<String>>();
+		for(int i=0;i<10;i++){
+			
+			Future<String> result=exex.submit(new TaskWithResult(i));
+			results.add(result);
+		}
+		for(Future<String> fs:results){
+			try {
+				System.out.println(fs.get());
+			} catch (Exception e) {
+				// TODO: handle exception
+			}finally{
+				exex.shutdown();
+			}
+			
+		}
+		
+	}
+
+}
